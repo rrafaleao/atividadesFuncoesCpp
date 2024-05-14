ab5eebf7c41bb38de909ee9626394ecb69691aad
rafael <162385496+rrafaleao@users.noreply.github.com>
2024-05-14 23:51:19 +0000

    atividadesFuncoesCpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//EXERCICIOS CONDICIONAIS
+
 //Verificação de numero par
 int verificaPar(){
   int num;
@@ -227,9 +229,235 @@ int calculadoraImc(){
     return false;
   }
 
+  //Cálculo de Recuperação: 
+  int calculoRecuperacao(){
+    float nota, notaRecup, presenca, notaFinal;
+
+    cout <<"Digite a nota do aluno: ";
+    cin >>nota;
+
+    cout<<"Digite a nota da recuperação do aluno: ";
+    cin>>notaRecup;
+
+    cout<<"Digite a porcentagem de presença do aluno: ";
+    cin>>presenca;
+
+    if (nota < 7 && nota >= 5) {
+        notaFinal = nota + notaRecup;
+        if (presenca >= 75 && notaFinal >= 10) {
+            cout<<"Aprovado" << endl;
+            cout<<"Nota final: " << notaFinal << endl;
+            cout<<"Presença: " << presenca << endl;
+        } else {
+            cout<<"Reprovado"<< endl;
+            cout<<"Nota final: " << notaFinal<<endl;
+            cout<<"Presença: " << presenca<<endl;
+        }
+    } else{
+        cout<<"Aluno não esta em recuperação, Nota: "<< nota << endl;
+    }
+
+    return false;
+  }
+
+  //Atividade sobre Arrays em C++
+
+  //Declaração e Impressão de Array (Utilizando For):
+
+  int declararArrayFor(){
+    int num[5] = {10,20,30,40,50};
+
+    for(int i = 0; i < 5; i++){
+      cout<<num[i]<<endl;
+    }
+
+    return false;
+  }
+
+  //Declaração e Impressão de Array (Utilizando While):
+
+  int declararArrayWhile(){
+    int num[5] = {5,4,3,2,1};
+    int i = 0;
+    while(i < 5){
+      cout<<num[i]<<endl;
+      i++;
+    }
+    return 0;
+  }
+
+  //Condicional com Array (Utilizando For):
+
+int condicionalArrayFor(){
+    int num[] = {1,4,8,12,5,6,7};
+    for(int i = 1; i < 7; i++){
+        if(num[i] % 2 == 0){
+            cout << num[i] << endl;
+        }
+    }
+    return false;
+}
+
+//Condicional com Array (Utilizando While):
+
+int condicionalArrayWhile(){
+    float num[] = {1.02,2.54,4.99,5.00,6.05,7.23,8.90,9.01};
+    int i = 0;
+
+    while(i < 8){
+        if(num[i] > 5){
+            cout << num[i] << endl;
+        }
+        i++;
+    }
+    return false;
+}
+
+//Atribuição de Valores e Impressão com For:
+
+int atribuicaoValorFor(){
+    int num[10];
+    cout << "Digite um número: " << endl;
+    cin >> num[0];
+    cout << "Digite um número: " << endl;
+    cin >> num[1];
+    cout << "Digite um número: " << endl;
+    cin >> num[2];
+    cout << "Digite um número: " << endl;
+    cin >> num[3];
+    cout << "Digite um número: " << endl;
+    cin >> num[4];
+    cout << "Digite um número: " << endl;
+    cin >> num[5];
+    cout << "Digite um número: " << endl;
+    cin >> num[6];
+    cout << "Digite um número: " << endl;
+    cin >> num[7];
+    cout << "Digite um número: " << endl;
+    cin >> num[8];
+    cout << "Digite um número: " << endl;
+    cin >> num[9];
+    cout<<endl;
+    int i = 0;
+
+    for(int i = 0; i < 10; i++){
+        cout << num[i] << endl;
+    }
+    return false;
+}
+//Impressão de Propriedades do Array:
+int impressaoPropriedades(){
+    char caracteres[] = {'@','#','+','/','&'};
+
+    for(int i = 0; i < 5; i++){
+        sizeof caracteres / sizeof caracteres[0];
+        cout << caracteres[i] << " ";
+        cout << "O array tem: " << sizeof caracteres << "caracteres. " << endl;
+    }
+    return false;
+}
+
+//Acesso a Elementos Específicos do Array:
+
+int elementosEspecificos(){
+    int array[] = {3,7,9,12,4,6,8,10};
+
+        cout << array[2] << endl;
+        cout << array[5] + array[7] << endl;
+        cout << array[1] - array[0] << endl;
+        if(array[3] > array[5]){
+            cout << array[3] << endl;
+        }
+        if(array[7] % 2 == 0){
+            cout << array[7] << endl;
+        }
+        if(array[3] + array[7] > 22){
+            cout << array[0] << endl;
+        }
+        return false;
+    }
+
+    //Entrada de Dados do Usuário e Cálculo da Média:
+
+    int dadosUsuario(){
+    float num[10], media;
+    cout <<"Digite um número: " << endl;
+    cin >> num[0];
+    cout <<"Digite um número: " << endl;
+    cin >> num[1];
+    cout <<"Digite um número: " << endl;
+    cin >> num[2];
+    cout <<"Digite um número: " << endl;
+    cin >> num[3];
+    cout <<"Digite um número: " << endl;
+    cin >> num[4];
+    cout <<"Digite um número: " << endl;
+    cin >> num[5];
+    cout <<"Digite um número: " << endl;
+    cin >> num[6];
+    cout <<"Digite um número: " << endl;
+    cin >> num[7];
+    cout <<"Digite um número: " << endl;
+    cin >> num[8];
+    cout <<"Digite um número: " << endl;
+    cin >> num[9];
+   media = ((num[0] + num[1] + num[2] + num[3] + num[4] + num[5] + num[6] + num[7] + num[8] + num[9]) / 10);
+
+   cout <<"a média dos numeros é: "<< media << endl;
+   return false;
+}
+
+//Cálculo da Soma dos Elementos do Array:
+
+int somaElementos(){
+    int num[] = {10,20,30,40,50,60,70,80,90,0};
+    int soma = 0;
+
+    for(int i = 0; i < 10; i++){
+        soma += num[i];
+        cout << soma << endl;;
+    }
+    return false;
+}
+
+//Condicional Aninhado com Array (Utilizando For):
+
+int condicionalAninhado(){
+    int num[] {10,2,3,9,43,52,81,9};
+    for(int i = 0; i < 8; i++){
+        if((num[i] % 3 == 0) || (num[i] > 10)){
+            cout << num[i] << endl;
+        }
+    return false;
+}
+}
+
+//Busca por Valor Máximo e Mínimo no Array:
+
+int buscaValorArray(){
+    int num[] {0,1,2,3,40,5,6,7,8,9,10,12};
+    int max = num[0];
+    int min = num[0];
+
+    for(int i = 0; i < 12; i++){
+        if(num[i] > max){
+            max = num[i];
+        }
+
+        if(num[i] < min){
+            min = num[i];
+        }
+    }        
+    cout <<"O maior numero do array é: " << max << endl;
+    cout <<"O menor numero do array é: " << min << endl;
+
+    return false;
+}
+
+
 
   int main(){
-    situacaoAcademica();
+    elementosEspecificos();
     return false;
   
   }
